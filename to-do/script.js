a8b7ed03dd59fb06cafe6e27e6aa47208913d700
Akash V <akasha09960@gmail.com>
2026-04-13 09:45:21 +0530

    to do app

---
+++ to-do/script.js
@@ -0,0 +1,11 @@
+var factor = "anyone"
+var fplayer = "same aas actor"
+var fmovie= "Anything"
+
+function favorite(){
+    console.log("actor :" + factor)
+    console.log("player :" + fplayer)
+    console.log("movie :" + fmovie)
+}
+
+favorite()
